e19416b3f958cb823f604fb2e19125e382aff7cc
Faris-kun <faris.ekananda20@gmail.com>
2019-04-08 20:03:12 +0700

    buat unit Pandas, tambah prosedur removeRow
    memudahkan hidup

--- example.pas
+++ example.pas
@@ -1,163 +1,36 @@
-Program Example38;
+Program Example42;
 
-uses sysutils, strutils;
-
-Type  
-  TCSVArr = record
-          Arr : array of array of string;
-          Row, Col : integer;
-          end;
-  TRow = record
-          Arr : array of string;
-          Col : integer;
-          end;
-
+uses pandas;
 var
-  TArr : TCSVArr;
-  TArrRow: TRow;
-  txt: string;
   i: integer;
-
-function Crypt(aText: string): string;
-const
-  PWD = 'hM!7JG$YeP9W~Smr*NK3&L2@cdy?Vx+M';
-var
-  i,j,len: integer;
+  TCSV: TCSVArr;
 begin
-  len := Length(aText);
-  SetLength(Crypt, len);
-  j:=1;
-  for i := 1 to len do
+  readCSV('Buku.csv',TCSV);
+  TWrite(TCSV);
+  while true do
   begin
-    Crypt[i] := Chr(Ord(aText[i]) + Ord(PWD[j]));
-    Inc(j);
-    if j>Length(PWD) then j:=1;
+    readln(i);
+    removeRow(TCSV,i);
+    TWrite(TCSV);
   end;
-end;
+  // //input
+  // readln(txt);
+  // while txt<>-999 do
+  // begin
+  //   //parse
+  //   TCSVRow:=CSVParser(txt);
+  //   //append row
+  //   addRow(TCSV,TCSVRow);
+  //   //print table
+  //   TWrite(TCSV);
+  //   //Tuliskan Col dan Row TCSV yang baru
+  //   writeln('Col: ',TCSV.Col,' and Row: ',TCSV.Row);
+  //   //input
+  //   readln(txt);
+  // end;
 
-procedure TWrite(TArr: TCSVArr);
-var
-  i, j: Integer;
-begin
-  for i:=Low(TArr.Arr) to High(Tarr.Arr) do
-  begin
-    Write('Arr[', i, ']:  ');
-    for j:=Low(TArr.Arr[i]) to High(TArr.Arr[i]) do
-      Write(TArr.Arr[i][j], '..');
-    WriteLn('|');
-  end;
-end;
-
-// abc
-// abc,def
-// abc,def,ghi,
-// ,abc,,,
-// awal: ""(quotation)char(cellbuf),(whitespace)
-// tengah: ""(quotation)char(cellbuf),(endcol/whitespace)
-// akhir: ""(quotation)/char(cellbuf/eol),(whitespace)
-
-function CSVParser(aText: string): TRow;
-const
-  Delim = ',';
-  Quote = '"';
-var
-  i,len,col: integer;
-  Quotation: boolean = false;
-  CellBuffer: string;
-begin
-  i:=1;
-  col:=0;
-  CellBuffer:='';
-  len:=length(aText);
-  while i<=len do
-  begin
-    SetLength(CSVParser.Arr,col+1);
-    if i=1 then
-    begin
-      if aText[i]=Delim then
-      begin
-        CSVParser.Arr[col]:=CellBuffer; //CellBuffer still whitespace
-        Inc(col); Inc(i);
-      end else
-      if aText[i]=Quote then
-      begin
-        Quotation:=true;
-        Inc(i);
-      end else
-      begin
-        CellBuffer:=CellBuffer+aText[i];
-        Inc(i);
-      end;
-    end else
-    begin
-      while Quotation do
-      begin
-        if aText[i]=Quote then
-        begin
-          CSVParser.Arr[col]:=CellBuffer;
-          CellBuffer:='';
-          Quotation:=false;
-          Inc(i,2); Inc(Col); //after end quotation always followed by delim
-        end else
-        begin
-          CellBuffer:=CellBuffer+aText[i];
-          Inc(i);
-        end;
-      end;
-      if aText[i]=Delim then
-      begin
-        CSVParser.Arr[col]:=CellBuffer;
-        CellBuffer:='';
-        Inc(i); Inc(Col);
-      end else
-      if aText[i]=Quote then
-      begin
-        Quotation:=true;
-        Inc(i);
-      end else
-      begin
-        CellBuffer:=CellBuffer+aText[i];
-        Inc(i);
-      end;
-    end;
-  end;
-  if aText[len]=Delim then
-  begin
-    CSVParser.Arr[col]:=CellBuffer;
-    CSVParser.Col:=col+1;
-  end else
-  if aText[len]=Quote then
-  begin
-    CSVParser.Col:=col;
-  end else
-  begin
-    CSVParser.Arr[col]:=CellBuffer;
-    CSVParser.Col:=col+1;
-  end;
-end;
-
-begin
-  TArr.Row:=0;
-  TArr.Col:=0;
-  readln(txt);
-  TArrRow:=CSVParser(txt);
-  Inc(TArr.Row); TArr.Col:=TArrRow.Col;
-  SetLength(TArr.Arr,TArr.Row,TArr.Col);
-  for i:=0 to TArrRow.Col-1 do
-    TArr.Arr[TArr.Row-1][i]:=TArrRow.Arr[i];
-  TWrite(TArr);
-  writeln(TArr.Col,TArr.Row);
-
-  readln(txt);
-  TArrRow:=CSVParser(txt);
-  Inc(TArr.Row); TArr.Col:=TArrRow.Col;
-  SetLength(TArr.Arr,TArr.Row,TArr.Col);
-  for i:=0 to TArrRow.Col-1 do
-    TArr.Arr[TArr.Row-1][i]:=TArrRow.Arr[i];
-  TWrite(TArr);
-  writeln(TArr.Col,TArr.Row);
-  // TArr[0][0]:=TArrRow[0];
-  // TArr[0][1]:=TArrRow[1];
-  // TArr[0][2]:=TArrRow[2];
-  // TWrite(TArr);
+  // TCSV[0][0]:=TCSVRow[0];
+  // TCSV[0][1]:=TCSVRow[1];
+  // TCSV[0][2]:=TCSVRow[2];
+  // TWrite(TCSV);
 End.
--- pandas.pas
+++ pandas.pas
@@ -1,7 +1,6 @@
-Program mainFaris;
+unit pandas;
 
-//uses Sysutils;
-
+interface
 Type  
   TRow = record
           Arr : array of string;
@@ -11,30 +10,28 @@ Type
           Arr : array of array of string;
           Row, Col : integer;
           end;
-
-var
-  TArr : TCSVArr; //format 2Dnya: [row,col]
-  TArrRow: TRow;
-  txt: string;
-  i: integer;
-
-function Crypt(aText: string): string;
 const
-  PWD = 'hM!7JG$YeP9W~Smr*NK3&L2@cdy?Vx+M';
-var
-  i,j,len: integer;
-begin
-  len := Length(aText);
-  SetLength(Crypt, len);
-  j:=1;
-  for i := 1 to len do
-  begin
-    Crypt[i] := Chr(Ord(aText[i]) + Ord(PWD[j]));
-    Inc(j);
-    if j>Length(PWD) then j:=1;
-  end;
-end;
+  Delim =',';
+  Quote ='"'; 
 
+//Write Table of TCSV for debugging
+procedure TWrite(TCSV: TCSVArr);
+//Sort TCSV with col as the pivot
+procedure sortCSV(var TCSV: TCSVArr; col: integer);
+//Parse string in csv format into TRow format
+function CSVParser(aText: string): TRow;
+//Append a row into the bottom-most TCSV
+procedure addRow(var TCSV: TCSVArr; aRow: TRow);
+//Remove specified row index
+procedure removeRow(var TCSV: TCSVArr; row: integer);
+//Read CSV File into TCSV
+procedure readCSV(const Filename: string; var TCSV: TCSVArr);
+//Convert a row from TCSV into string with csv format
+function CSVBuilder(aRow: TCSVArr; row: integer): string;
+//Overwrite TCSV to a CSV File
+procedure writeCSV(const Filename: string; TCSV: TCSVArr);
+
+implementation
 // Tulis tabel tipe TCSVArr
 procedure TWrite(TCSV: TCSVArr);
 var
@@ -49,20 +46,12 @@ begin
   end;
 end;
 
-// abc
-// abc,def
-// abc,def,ghi,
-// ,abc,,,
-// awal: ""(quotation)char(cellbuf),(whitespace)
-// tengah: ""(quotation)char(cellbuf),(endcol/whitespace)
-// akhir: ""(quotation)/char(cellbuf/eol),(whitespace)
+procedure sortCSV(var TCSV: TCSVArr; col: integer);
+
 
 //Parse String menjadi bentuk TRow
 function CSVParser(aText: string): TRow;
 //Deklarasi delimiter dan quote char
-const
-  Delim = ',';
-  Quote = '"';
 var
   i,len,col: integer;
   Quotation: boolean = false;
@@ -77,53 +66,34 @@ begin
   begin
     //Ubah Ukuran Arraynya setiap pengulangan while
     SetLength(CSVParser.Arr,col+1);
-    if i=1 then //cek karakter pertama di string aText
+    while Quotation do //Prosedur quotation, hanya berhenti ketika menemui quote berikutnya dan mengabaikan tanda delimiter
     begin
-      if aText[i]=Delim then //Kalau langsung ketemu delimiter artinya kolom pertamanya baris kosong
+      if aText[i]=Quote then
       begin
-        CSVParser.Arr[col]:=CellBuffer; //CellBuffer still ''
-        Inc(col); Inc(i); //Setiap pemasukan ke result CSVParser.Arr, tambahkan kolom yang ingin diproses
-      end else
-      if aText[i]=Quote then //Kalau langsung ketemu quote, switch flag quotation
-      begin
-        Quotation:=true;
-        Inc(i);
+        CSVParser.Arr[col]:=CellBuffer;
+        CellBuffer:='';
+        Quotation:=false;
+        Inc(i,2); Inc(Col); //after end quotation always followed by delim
       end else
       begin
-        CellBuffer:=CellBuffer+aText[i]; //Kalau langsung ketemu karakter biasa
+        CellBuffer:=CellBuffer+aText[i];
         Inc(i);
       end;
+    end;
+    if aText[i]=Delim then //Pas di tengah2 ketemu delim. CellBuffer sudah pasti mengandung isi cell satu kolom.
+    begin
+      CSVParser.Arr[col]:=CellBuffer; //Assign CellBuffer
+      CellBuffer:=''; //Reset CellBuffer
+      Inc(i); Inc(Col);
+    end else
+    if aText[i]=Quote then //Pas di tengah2 ketemu quote
+    begin
+      Quotation:=true;
+      Inc(i);
     end else
     begin
-      while Quotation do //Prosedur quotation, hanya berhenti ketika menemui quote berikutnya dan mengabaikan tanda delimiter
-      begin
-        if aText[i]=Quote then
-        begin
-          CSVParser.Arr[col]:=CellBuffer;
-          CellBuffer:='';
-          Quotation:=false;
-          Inc(i,2); Inc(Col); //after end quotation always followed by delim
-        end else
-        begin
-          CellBuffer:=CellBuffer+aText[i];
-          Inc(i);
-        end;
-      end;
-      if aText[i]=Delim then //Pas di tengah2 ketemu delim. CellBuffer sudah pasti mengandung isi cell satu kolom.
-      begin
-        CSVParser.Arr[col]:=CellBuffer; //Assign CellBuffer
-        CellBuffer:=''; //Reset CellBuffer
-        Inc(i); Inc(Col);
-      end else
-      if aText[i]=Quote then //Pas di tengah2 ketemu quote
-      begin
-        Quotation:=true;
-        Inc(i);
-      end else
-      begin
-        CellBuffer:=CellBuffer+aText[i]; //Pas di tengah2 ketemu karakter biasa, masukkan ke cellBuffer
-        Inc(i);
-      end;
+      CellBuffer:=CellBuffer+aText[i]; //Pas di tengah2 ketemu karakter biasa, masukkan ke cellBuffer
+      Inc(i);
     end;
   end;
   //Cek karakter terakhir aText apa. Trus, assign jumlah kolomnya ke CSVParser.Col
@@ -144,12 +114,25 @@ end;
 
 //Prosedur penambahan Row (AppendRow) pada TCSV (Table CSV)
 procedure addRow(var TCSV: TCSVArr; aRow: TRow);
+var
+  i: integer;
 begin
   Inc(TCSV.Row); //Tambahkan Rownya TCSV
   if TCSV.Col=0 then TCSV.Col:=aRow.Col; //Kalau TCSV belum punya Col, assign Col sesuai dgn Col aRow
   SetLength(TCSV.Arr,TCSV.Row,TCSV.Col); //SetLength Array sesuai dgn yg sudah ditentukan Row dan Colnya tadi
   for i:=0 to TCSV.Col-1 do
     TCSV.Arr[TCSV.Row-1][i]:=aRow.Arr[i]; //Isi Row terbawah TCSV dgn aRow
+end;
+
+//Prosedur penghapusan row pada index tertentu
+procedure removeRow(var TCSV: TCSVArr; row: integer);
+var
+  i: integer;
+begin
+  for i:=row+1 to TCSV.Row-1 do
+    TCSV.Arr[i-1] := TCSV.Arr[i];
+  Dec(TCSV.Row);
+  SetLength(TCSV.Arr,TCSV.Row,TCSV.Col);
 end;
 
 procedure readCSV(const Filename: string; var TCSV: TCSVArr);
@@ -173,12 +156,8 @@ begin
 end;
 
 function CSVBuilder(aRow: TCSVArr; row: integer): string;
-const
-  Delim = ',';
-  Quote = '"';
 var
   col: integer;
-  cell: string;
 begin
   CSVBuilder:='';
   for col:=0 to aRow.Col-2 do
@@ -193,7 +172,13 @@ begin
       CSVBuilder:= CSVBuilder + aRow.Arr[row][col] + Delim;
     end;
   end;
-  CSVBuilder:= CSVBuilder + aRow.Arr[row][aRow.Col-1];
+  if Pos(Delim,aRow.Arr[row][col])<>0 then
+  begin
+    CSVBuilder:= CSVBuilder + Quote + aRow.Arr[row][aRow.Col-1] + Quote;
+  end else
+  begin
+    CSVBuilder:= CSVBuilder + aRow.Arr[row][aRow.Col-1];
+  end;
 end;
 
 procedure writeCSV(const Filename: string; TCSV: TCSVArr);
@@ -213,32 +198,4 @@ for i:=Low(TCSV.Arr) to High(TCSV.Arr) do //for loop buat rownya
 Close(tfOut);
 end;
 
-begin
-  readCSV('Buku.csv',TArr);
-  TWrite(TArr);
-  while true do
-  begin
-    readln(i);
-    writeln(CSVBuilder(TArr,i));
-  end;
-  // //input
-  // readln(txt);
-  // while txt<>-999 do
-  // begin
-  //   //parse
-  //   TArrRow:=CSVParser(txt);
-  //   //append row
-  //   addRow(TArr,TArrRow);
-  //   //print table
-  //   TWrite(TArr);
-  //   //Tuliskan Col dan Row TCSV yang baru
-  //   writeln('Col: ',TArr.Col,' and Row: ',TArr.Row);
-  //   //input
-  //   readln(txt);
-  // end;
-
-  // TArr[0][0]:=TArrRow[0];
-  // TArr[0][1]:=TArrRow[1];
-  // TArr[0][2]:=TArrRow[2];
-  // TWrite(TArr);
-End.
+end.
