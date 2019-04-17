c038ffb8ac9c98d72a0daccac0cc2268460646c1
ShafaAM <shfamrsya@gmail.com>
2019-04-17 22:27:15 +0700

    ya gitulah ya

--- test.pas
+++ test.pas
@@ -1,20 +1,21 @@
-Program Example10;
+Program Example5;
 uses Crt;
 
-{ Program to demonstrate the InsLine function. }
+{ Program to demonstrate the Window function. }
 
 begin
   ClrScr;
-  WriteLn;
-  WriteLn('Line 1');
-  WriteLn('Line 2');
-  WriteLn('Line 2');
-  WriteLn('Line 3');
-  WriteLn;
-  WriteLn('Oops, Line 2 is listed twice,',
-          ' let''s delete the line at the cursor postion');
-  GotoXY(1,3);
+  WriteLn('Creating a window from 30,10 to 50,20');
+  Window(30,10,50,20);
+  WriteLn('We are now writing in this small window we just created, we '+
+          'can''t get outside it when writing long lines like this one');
+  Write('Press any key to clear the window');
   ReadKey;
-  DelLine;
-  GotoXY(1,10);
+  ClrScr;
+  Write('The window is cleared, press any key to restore to fullscreen');
+  ReadKey;
+{Full Screen is 80x25}
+  Window(1,1,80,25);
+  Clrscr;
+  Writeln('Back in Full Screen');
 end.
